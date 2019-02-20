d2a7e82d68f7758971c17bccc882a7f020a140c0
Momen Abdelkarim <momen.abdelkarim@gmail.com>
2019-02-19 20:23:54 -0700

    Implemented functionality to return a history of issued calculations. Corrected some documentation.

--- src/cse360assign2/Calculator.java
+++ src/cse360assign2/Calculator.java
@@ -13,12 +13,15 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
+	private String history;
 
 	/**
-	 * Sole constructor. Initializes the total (of the calculations) to 0.
+	 * Sole constructor that initializes the total (of the calculations) to 0
+     * and the history of the calculator at "0".
 	 */
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 
 	/**
@@ -30,42 +33,52 @@ public class Calculator {
 	}
 
 	/**
-	 * Adds an integer to the total (of the issued calculations so far) stored in
-	 * the 'total' variable.
+	 * Adds an integer to the total (of the issued calculations so far) stored
+	 * in the 'total' variable. Also, appends said operation to the calculator
+	 * history stored in the 'history' variable.
 	 * @param value the integer, taken from user input, that is to be added to
 	 *                 the total.
 	 */
 	public void add (int value) {
 		total = total + value;
+
+		history = history + " + " + value;
 		
 	}
 
 	/**
 	 * Subtracts an integer from the total (of the issued calculations so far)
-	 * stored in the 'total' variable.
+	 * stored in the 'total' variable. Also, appends said operation to the
+	 * calculator history stored in the 'history' variable.
 	 * @param value the integer, taken from user input, that is to be subtracted
 	 *                 from the total.
 	 */
 	public void subtract (int value) {
 		total = total - value;
+
+		history = history + " - " + value;
 		
 	}
 
 	/**
 	 * Multiplies the total (from the issued calculations so far) stored in the
-	 * total variable by an integer.
+	 * total variable by an integer. Also, appends said operation to the
+	 * calculator history stored in the 'history' variable.
 	 * @param value the integer, taken from user input, that multiplies the
 	 *                 total.
 	 */
 	public void multiply (int value) {
 		total = total * value;
+
+		history = history + " * " + value;
 		
 	}
 
 	/**
 	 * Divides the total (from the issued calculations so far) stored in the
 	 * total variable by an integer. If the given 'value' is 0, the total is set
-	 * to zero.
+	 * to zero. Also, appends said operation to the calculator history stored
+	 * in the 'history' variable.
 	 * @param value the integer, taken from user input, that divides the
 	 *                 total.
 	 */
@@ -76,14 +89,16 @@ public class Calculator {
 		else {
 			total = 0;
 		}
+
+		history = history + " / " + value;
 	}
 
 	/**
-	 * In practice, returns the history of previously issued calculations,
-	 * starting from 0 to the last calculation.
-	 * @return 	an empty string for now
+	 * Returns the history of previously issued calculations, starting from 0
+	 * to the last calculation.
+	 * @return 	a history of the previously issues calculations.
 	 */
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
--- src/cse360assign2/Main.java
+++ src/cse360assign2/Main.java
@@ -5,12 +5,27 @@ public class Main {
    public static void main(String[] args){
         Calculator x = new Calculator();
 
-        x.add(-3);
-        x.subtract(3);
+        x.add(0);
+        x.subtract(0);
         x.divide(3);
-        x.multiply(3);
+        x.multiply(1);
+        x.add(1);
 
         System.out.println(x.getTotal());
-        //System.out.println(x.getHistory());
+        System.out.println(x.getHistory() + '\n');
+
+       Calculator y = new Calculator();
+
+       y.add(4);
+       y.subtract(2);
+       y.multiply(2);
+       y.add(5);
+       y.divide(4);
+       y.add(0);
+
+       System.out.println(y.getTotal());
+       System.out.println(y.getHistory());
+
+
     }
 }
